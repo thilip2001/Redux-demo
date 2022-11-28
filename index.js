7b570ef7fe0fc77aeccd7deb5017d8ddb9b5f54f
Thiliban <thilip2017@gmail.com>
2022-11-28 15:49:35 +0530

    store added

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-console.log("from index.js");
-console.log("Hello All, I am Thiliban");
+const redux = require("redux");
+// import { legacy_createStore as createStore } from 'redux';
+
+const createStore = redux.legacy_createStore;
+
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "Action from Redux",
+  };
+}
+
+// (previous state, action) => newState
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log("Initial State", store.getState());
