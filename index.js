69dab302f7ca0624eb0a485c7e39ed296dd1111b
Thiliban <thilip2017@gmail.com>
2022-11-29 22:29:17 +0530

    multiple reduer

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const redux = require("redux");
 // import { legacy_createStore as createStore } from 'redux';
 
 const createStore = redux.legacy_createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
@@ -12,13 +14,45 @@ function buyCake() {
   };
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
 // (previous state, action) => newState
 
 const initialState = {
   numberOfCakes: 10,
+  numberOfIceCreams: 20,
 };
 
-const reducer = (state = initialState, action) => {
+const initialCakeState = {
+  numberOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numberOfIceCreams: 20,
+};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numberOfCakes: state.numberOfCakes - 1,
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state,
+//         numberOfIceCreams: state.numberOfIceCreams - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -30,7 +64,24 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numberOfIceCreams: state.numberOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
 
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() =>
@@ -42,8 +93,11 @@ store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch({ type: BUY_CAKE });
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
 unsubscribe();
 
-console.log(store);
-console.log("Initial State", store.getState());
+// console.log(store);
+// console.log("Initial State", store.getState());
